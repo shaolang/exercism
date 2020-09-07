9141b566425a099671ec7cf8527ff08ae1964f34
shaolang <shaolang@ymail.com>
2020-09-07 08:09:14 +0800

    Simplify reverse string with no intermediary collection

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut s: Vec<&str> = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
-    s.reverse();
-    s.join("")
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
 }
