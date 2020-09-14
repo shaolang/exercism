d0bfd7c0e7c19b907d9544027a8347dac57eb9ad
shaolang <shaolang@ymail.com>
2020-09-15 07:27:26 +0800

    Simplify sum of multiples using Stream.filter

--- elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -5,16 +5,11 @@ defmodule SumOfMultiples do
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
     1..(limit - 1)
-    |> Stream.map(&(multiple(&1, factors)))
+    |> Stream.filter(&(multiple?(&1, factors)))
     |> Enum.sum
   end
 
-  defp multiple(n, factors) do
-    Enum.reduce_while(factors, 0, fn factor, _ ->
-      case rem(n, factor) do
-        0 -> {:halt, n}
-        _ -> {:cont, 0}
-      end
-    end)
+  defp multiple?(n, factors) do
+    Enum.any?(factors, fn f -> rem(n, f) == 0 end)
   end
 end
