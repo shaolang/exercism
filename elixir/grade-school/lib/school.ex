308920855a6ebe7d48f2ed4d86209cd07fed07f0
shaolang <shaolang@ymail.com>
2020-09-16 07:14:29 +0800

    Complete grade school exercise from Elixir track

---
+++ elixir/grade-school/lib/school.ex
@@ -0,0 +1,33 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(map, String.t(), integer) :: map
+  def add(db, name, grade) do
+    Map.update(db, grade, [name], fn others -> [name | others] end)
+  end
+
+  @doc """
+  Return the names of the students in a particular grade.
+  """
+  @spec grade(map, integer) :: [String.t()]
+  def grade(db, grade) do
+    Map.get(db, grade, [])
+  end
+
+  @doc """
+  Sorts the school by grade and name.
+  """
+  @spec sort(map) :: [{integer, [String.t()]}]
+  def sort(db) do
+    db
+    |> Enum.sort
+    |> Enum.map(fn {grade, students} -> {grade, Enum.sort(students)} end)
+  end
+end
