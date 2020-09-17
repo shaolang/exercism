862b902c7bf32b993bacb6543a16b424b7a18502
shaolang <shaolang@ymail.com>
2020-09-18 07:24:50 +0800

    Use length/1 instead of Enum.count/1 in hamming solution

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -9,7 +9,7 @@ defmodule Hamming do
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
   def hamming_distance(strand1, strand2) do
-    case Enum.count(strand1) == Enum.count(strand2) do
+    case length(strand1) == length(strand2) do
       false -> {:error, "Lists must be the same length"}
       true ->
         {:ok, strand1
