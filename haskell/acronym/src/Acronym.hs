0789083109d0a58637256b29ef1728b5851aad8b
shaolang <shaolang@ymail.com>
2020-10-18 11:45:56 +0800

    Simplify Haskell acronym exercise

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,18 +1,16 @@
 module Acronym (abbreviate) where
 
-import Data.Char  (isLetter, isLower, isSeparator, isUpper, toUpper)
+import Data.Char  (isLower, isUpper, toUpper)
 
 abbreviate :: String -> String
-abbreviate xs = map (\(c:_) -> toUpper c) $ words xs'
-  where xs' = split_camelcase $ filter (\c -> isLetter c || isSeparator c) $ replace_dashes xs
+abbreviate xs = map (toUpper . head) $ ws
+  where ws = words $ replaceWithSpace xs
 
-replace_dashes :: String -> String
-replace_dashes [] = []
-replace_dashes ('-':cs) = ' ':replace_dashes cs
-replace_dashes (c:cs) = c:replace_dashes cs
-
-split_camelcase :: String -> String
-split_camelcase (x:y:z:cs) = if isLower x && isUpper y && isLower z
-                             then x:' ':(split_camelcase $ y:z:cs)
-                             else x:(split_camelcase $ y:z:cs)
-split_camelcase cs = cs
+replaceWithSpace :: String -> String
+replaceWithSpace [] = []
+replaceWithSpace ('_':cs) = ' ':replaceWithSpace cs
+replaceWithSpace ('-':cs) = ' ':replaceWithSpace cs
+replaceWithSpace (x:y:z:cs) = if isLower x && isUpper y && isLower z
+                              then x:' ':replaceWithSpace (y:z:cs)
+                              else x:replaceWithSpace (y:z:cs)
+replaceWithSpace (c:cs) = c:replaceWithSpace cs
