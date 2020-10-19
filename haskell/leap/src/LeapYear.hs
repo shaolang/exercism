d092bc815e3d9ce6e0892ca09433c200e5c10a6d
shaolang <shaolang@ymail.com>
2020-10-19 18:05:11 +0800

    Check for eveness in leap year Haskell exercise.
    Mentor is really, really helpful.

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,5 +1,10 @@
 module LeapYear (isLeapYear) where
 
+import Data.Bits
+
 isLeapYear :: Integer -> Bool
-isLeapYear year = result
+isLeapYear year = isEven year && result
   where result = (year `rem` 4 == 0 && year `rem` 100 /= 0) || year `rem` 400 == 0
+
+isEven :: Integer -> Bool
+isEven n = (n .&. 1) == 0
