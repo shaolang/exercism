002ef9172e76364373a23bc4ef217a0be2631a33
shaolang <shaolang@ymail.com>
2020-10-19 18:07:20 +0800

    Rename result variable in leap year for clarity

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -3,8 +3,9 @@ module LeapYear (isLeapYear) where
 import Data.Bits
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = isEven year && result
-  where result = (year `rem` 4 == 0 && year `rem` 100 /= 0) || year `rem` 400 == 0
+isLeapYear year = isEven year && divisbleBy4Not100
+  where divisbleBy4Not100 = (year `rem` 4 == 0 && year `rem` 100 /= 0) ||
+                            year `rem` 400 == 0
 
 isEven :: Integer -> Bool
 isEven n = (n .&. 1) == 0
