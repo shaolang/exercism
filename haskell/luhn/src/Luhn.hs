ea70513f066b004cd2a5fb2f0e2d8f2e45bf2920
shaolang <shaolang@ymail.com>
2020-10-20 21:47:59 +0800

    Simplify luhn with Data.List zipWith and cycle functions

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -1,24 +1,14 @@
 module Luhn (isValid) where
 
 import Data.Char    (digitToInt, isNumber, isSpace)
-import Data.List    (partition)
 
 isValid :: String -> Bool
 isValid n = length ns > 1 && digitsOnly && total `rem` 10 == 0
-  where digitsOnly    = all (\c -> isNumber c || isSpace c) n
-        ns            = map digitToInt $ filter isNumber n
-        (secs, fsts)  = partitionEveryOther ns
-        total         = sum fsts + (sum $ map luhnDouble secs)
+  where digitsOnly  = all (\c -> isNumber c || isSpace c) n
+        ns          = map digitToInt $ filter isNumber n
+        nss         = zipWith (*) (cycle [1, 2]) (reverse ns)
+        total       = sum $ map capAtNine nss
 
-isEven :: Int -> Bool
-isEven n = n `rem` 2 == 0
-
-partitionEveryOther :: [Int] -> ([Int], [Int])
-partitionEveryOther ns = (map snd ys, map snd xs)
-  where (xs, ys) = partition (\(i, _) -> isEven i) $ zip [0..] $ reverse ns
-
-luhnDouble :: Int -> Int
-luhnDouble n = if result > 9
-               then result - 9
-               else result
-  where result = n * 2
+capAtNine :: Int -> Int
+capAtNine n | n > 9     = n - 9
+            | otherwise = n
