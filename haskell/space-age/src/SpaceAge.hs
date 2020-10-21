53db8631518cd01b5be5a332fc69d1efdbabe78f
shaolang <shaolang@ymail.com>
2020-10-21 15:01:21 +0800

    Another update to space age solution after mentor's comments

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -9,18 +9,15 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
-ageOn :: Planet -> Float -> Float
-ageOn planet seconds = seconds / divisor planet
-
 earthSecondsPerYear :: Float
 earthSecondsPerYear = 31557600
 
-divisor :: Planet -> Float
-divisor Mercury = earthSecondsPerYear * 0.2408467
-divisor Venus   = earthSecondsPerYear * 0.61519726
-divisor Earth   = earthSecondsPerYear
-divisor Mars    = earthSecondsPerYear * 1.8808158
-divisor Jupiter = earthSecondsPerYear * 11.862615
-divisor Saturn  = earthSecondsPerYear * 29.447498
-divisor Uranus  = earthSecondsPerYear * 84.016846
-divisor Neptune = earthSecondsPerYear * 164.79132
+ageOn :: Planet -> Float -> Float
+ageOn Mercury seconds = seconds / (earthSecondsPerYear * 0.2408467)
+ageOn Venus   seconds = seconds / (earthSecondsPerYear * 0.61519726)
+ageOn Earth   seconds = seconds / earthSecondsPerYear
+ageOn Mars    seconds = seconds / (earthSecondsPerYear * 1.8808158)
+ageOn Jupiter seconds = seconds / (earthSecondsPerYear * 11.862615)
+ageOn Saturn  seconds = seconds / (earthSecondsPerYear * 29.447498)
+ageOn Uranus  seconds = seconds / (earthSecondsPerYear * 84.016846)
+ageOn Neptune seconds = seconds / (earthSecondsPerYear * 164.79132)
