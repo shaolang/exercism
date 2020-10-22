b8ea88b1631a4c1db571ff35a119c1faf20495cc
shaolang <shaolang@ymail.com>
2020-10-22 20:41:05 +0800

    Update pangram solution from Haskell track based on mentor's feedback

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,12 +1,12 @@
 module Pangram (isPangram) where
 
 import Data.Char  (isAsciiLower, toLower)
-import Data.List  (group, sort)
+import Data.List  (intersect)
 
 isPangram :: String -> Bool
-isPangram text = alphabet == map head cs
+isPangram text = length cs == 26
   where letters = filter isAsciiLower $ map toLower text
-        cs      = group $ sort letters
+        cs      = alphabet `intersect` letters
 
 alphabet :: [Char]
 alphabet = ['a' .. 'z']
