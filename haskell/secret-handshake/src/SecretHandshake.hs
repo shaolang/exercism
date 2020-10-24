cb25a5f798540732d032aed1ac9c8ab73036ec1f
shaolang <shaolang@ymail.com>
2020-10-24 09:28:45 +0800

    Simplify secret handshake by using list comprehensive

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -1,31 +1,13 @@
 module SecretHandshake (handshake) where
 
-import Data.Bits ((.&.), shift)
+import Data.Bits (testBit)
+
+actions :: [String]
+actions = ["wink", "double blink", "close your eyes", "jump"]
 
 handshake :: Int -> [String]
 handshake n = steps
-  where xs = map (\f -> f n) [wink, doubleBlink, closeEyes, jump]
-        xs' = filter (not . null) xs
-        steps | rev n == "" = xs'
-              | otherwise   = reverse xs'
-
-wink :: Int -> String
-wink = action "wink" 0
-
-doubleBlink :: Int -> String
-doubleBlink = action "double blink" 1
-
-closeEyes :: Int -> String
-closeEyes = action "close your eyes" 2
-
-jump :: Int -> String
-jump = action "jump" 3
-
-rev :: Int -> String
-rev = action "rev" 4
-
-action :: String -> Int -> Int -> String
-action xs bitShift n = result
-  where b = 1 `shift` bitShift
-        result | b .&. n > 0 = xs
-               | otherwise   = ""
+  where xs = zip actions [testBit n x | x <- [0 .. 3::Int]]
+        xs' = map fst $ filter snd xs
+        steps | testBit n 4 = reverse xs'
+              | otherwise   = xs'
