6bb28a604c8406260f8463ae6d55375f98a5644b
shaolang <shaolang@ymail.com>
2020-11-03 07:12:19 +0800

    Update solution to bob based on mentor's feedback

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -5,9 +5,11 @@ import Data.Char (isAlpha, isSpace, isUpper)
 responseFor :: String -> String
 responseFor xs
   | xs' == ""                                       = "Fine. Be that way!"
-  | xss /= "" && all isUpper xss && last xs' == '?' = "Calm down, I know what I'm doing!"
-  | last xs' == '?'                                 = "Sure."
-  | all isUpper xss && xss /= ""                    = "Whoa, chill out!"
+  | upperCaseOnly && isQuestion = "Calm down, I know what I'm doing!"
+  | isQuestion                  = "Sure."
+  | upperCaseOnly                     = "Whoa, chill out!"
   | otherwise                                       = "Whatever."
   where xs' = filter (not . isSpace) xs
         xss = filter isAlpha xs
+        isQuestion    = last xs' == '?'
+        upperCaseOnly = all isUpper xss && xss /= ""
