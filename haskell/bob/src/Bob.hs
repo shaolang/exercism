59cdfa7fb0d1af1abe7623aeaacd36f857288752
shaolang <shaolang@ymail.com>
2020-11-03 15:35:53 +0800

    Fix indentation to bob solution

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -4,11 +4,11 @@ import Data.Char (isAlpha, isSpace, isUpper)
 
 responseFor :: String -> String
 responseFor xs
-  | xs' == ""                                       = "Fine. Be that way!"
+  | xs' == ""                   = "Fine. Be that way!"
   | upperCaseOnly && isQuestion = "Calm down, I know what I'm doing!"
   | isQuestion                  = "Sure."
-  | upperCaseOnly                     = "Whoa, chill out!"
-  | otherwise                                       = "Whatever."
+  | upperCaseOnly               = "Whoa, chill out!"
+  | otherwise                   = "Whatever."
   where xs' = filter (not . isSpace) xs
         xss = filter isAlpha xs
         isQuestion    = last xs' == '?'
