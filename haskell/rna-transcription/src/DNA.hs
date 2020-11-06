6c6406894a926ca012f656985dcc3851d98cc3d1
shaolang <shaolang@ymail.com>
2020-11-06 10:48:31 +0800

    Update solution to rna-transcription based on mentor's feedback

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,12 +1,16 @@
 module DNA (toRNA) where
 
-import Data.Maybe   (catMaybes)
+import Data.Either  (partitionEithers)
 
 toRNA :: String -> Either Char String
-toRNA xs
-  | unknowns /= []  = Left $ head unknowns
-  | otherwise       = Right $ catMaybes [lookup x d2r | x <- xs]
-  where unknowns = [ x | x <- xs, x `notElem` "GCTA"]
+toRNA xs = if length lefts > 0
+           then Left $ head lefts
+           else Right rights
+  where (lefts, rights) = partitionEithers [d2r x | x <- xs]
 
-d2r :: [(Char, Char)]
-d2r = [('G', 'C'), ('C', 'G'), ('T', 'A'), ('A', 'U')]
+d2r :: Char -> Either Char Char
+d2r 'G' = Right 'C'
+d2r 'C' = Right 'G'
+d2r 'T' = Right 'A'
+d2r 'A' = Right 'U'
+d2r c   = Left c
