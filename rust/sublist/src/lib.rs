0a108a01017f65ec3ebd2091913bf5b8b632f459
shaolang <shaolang@ymail.com>
2021-01-08 20:16:52 +0800

    Run cargo fmt

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -11,7 +11,13 @@ pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison 
         (0, 0) => Comparison::Equal,
         (0, _) => Comparison::Sublist,
         (_, 0) => Comparison::Superlist,
-        (n, m) if n == m => if first_list == second_list { Comparison::Equal } else { Comparison::Unequal },
+        (n, m) if n == m => {
+            if first_list == second_list {
+                Comparison::Equal
+            } else {
+                Comparison::Unequal
+            }
+        }
         (n, m) if n < m => is_sublist(first_list, second_list, Comparison::Sublist),
         _ => is_sublist(second_list, first_list, Comparison::Superlist),
     }
@@ -22,7 +28,7 @@ fn is_sublist<T: PartialEq>(small: &[T], big: &[T], true_value: Comparison) -> C
 
     for i in 0..=(big.len() - n) {
         if small == &big[i..(i + n)] {
-            return true_value
+            return true_value;
         }
     }
 
